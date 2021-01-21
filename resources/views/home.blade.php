2be7a14fc8061433b748534ed3014d2beacb89da
Antonia P <direiellie@gmail.com>
2021-01-21 14:42:05 +0100

    Creazione Navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,16 @@
+@extends('layouts.main')
 
+@section('content')
+    <div class='container mb-5'>
+        <h1>
+            SCHOOL HOME PAGE HERE
+        </h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id debitis distinctio ad cupiditate, voluptas odit accusamus iste, alias commodi quod harum quaerat doloremque ratione error expedita veritatis in, itaque numquam?</p>
+
+    </div>
+    
+@endsection
 
 
 
--- resources/views/layouts/main-blade.php
+++ resources/views/layouts/main-blade.php
@@ -7,13 +7,26 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
     </head>
     <body>
+        <!--HEADER-->
 
+            @include('partials.header')
+    
+        <!--MAIN-->
 
+        <main>
+            @yield('content')
+        </main>
+
+        <!--FOOTER-->
+   
+            @include('partials.footer)
         
     </body>
 </html>

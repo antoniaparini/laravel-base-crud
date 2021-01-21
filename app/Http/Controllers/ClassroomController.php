8efbe1b487215ed6b37416f5af7d1318984e5d5b
Antonia P <direiellie@gmail.com>
2021-01-21 19:38:37 +0100

    Create

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -26,7 +26,7 @@ class ClassroomController extends Controller
      */
     public function create()
     {
-        //
+        return view('classrooms.create');
     }
 
     /**
@@ -37,7 +37,7 @@ class ClassroomController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return 'Funzione ST';
     }
 
     /**
@@ -48,7 +48,7 @@ class ClassroomController extends Controller
      */
     public function show($id)
     {
-        //
+        return 'Funzione SH';
     }
 
     /**
